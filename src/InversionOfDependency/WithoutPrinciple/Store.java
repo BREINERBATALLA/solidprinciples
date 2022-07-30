9c9fff1f8ca8d76b0934bee9148cdd3d56be55a7
Breiner Batalla <47467079+BREINERBATALLA@users.noreply.github.com>
2022-07-30 11:01:17 -0500

    Update Store.java

--- src/InversionOfDependency/WithoutPrinciple/Store.java
+++ src/InversionOfDependency/WithoutPrinciple/Store.java
@@ -3,7 +3,7 @@ package InversionOfDependency.WithoutPrinciple;
 public class Store {
     private MysqlDB mysql;
 
-    //High coupling, we'll have a trouble if we change the SGDB,
+    //High coupling, we'll have a trouble if we change the DB,
     //We must to have a low coupling for the external libraries and frameworks,
     //because ours system must not depends on low level modules
     public Store(MysqlDB mysql){
